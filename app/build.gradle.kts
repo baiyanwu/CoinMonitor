3d7b1a0b90c010822b37a0186ed3a88665280cc4
baiyanwu <baiyanwuxu@gmail.com>
2026-03-31 19:26:04 +0800

    Bump version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "io.baiyanwu.coinmonitor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
